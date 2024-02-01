43a5ff7c7ac0ced611d359a5793b9cdc7bb03833
Kuba <thekubajacob@gmail.com>
2024-02-01 14:40:52 +0100

    navbar layout test

--- components/shared/navbar/NavBar.tsx
+++ components/shared/navbar/NavBar.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 sm:px-12 dark:shadow-none">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -33,7 +33,7 @@ const NavBar = () => {
             }}
           />
         </SignedIn>
-        MobileNav
+        MobileNavTest
       </div>
     </nav>
   );
