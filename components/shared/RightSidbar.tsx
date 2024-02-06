05b5782d3595b6daf757c2f23cb06c2f19dca619
Kuba <thekubajacob@gmail.com>
2024-02-06 16:11:25 +0100

    homepage ui done

--- components/shared/RightSidbar.tsx
+++ components/shared/RightSidbar.tsx
@@ -5,25 +5,25 @@ import RenderTag from "./RenderTag";
 
 const hotQuestions = [
   {
-    _id: 1,
+    _id: "1",
     title:
       "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
   },
-  { _id: 2, title: "Is it only me or the font is bolder than necessary?" },
-  { _id: 3, title: "Can I get the course for free?" },
-  { _id: 4, title: "Redux Toolkit Not Updating State as Expected" },
+  { _id: "2", title: "Is it only me or the font is bolder than necessary?" },
+  { _id: "3", title: "Can I get the course for free?" },
+  { _id: "4", title: "Redux Toolkit Not Updating State as Expected" },
   {
-    _id: 5,
+    _id: "5",
     title: "Async/Await Function Not Handling Errors Properly",
   },
 ];
 
 const popularTags = [
-  { _id: 1, name: "javascript", totalQuestions: 3 },
-  { _id: 1, name: "react", totalQuestions: 2 },
-  { _id: 1, name: "next", totalQuestions: 5 },
-  { _id: 1, name: "vue", totalQuestions: 7 },
-  { _id: 1, name: "redux", totalQuestions: 5 },
+  { _id: "1", name: "javascript", totalQuestions: 3 },
+  { _id: "1", name: "react", totalQuestions: 2 },
+  { _id: "1", name: "next", totalQuestions: 5 },
+  { _id: "1", name: "vue", totalQuestions: 7 },
+  { _id: "1", name: "redux", totalQuestions: 5 },
 ];
 
 const RightSidbar = () => {
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,55 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const timeDifference = now.getTime() - createdAt.getTime();
+
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(days / 365);
+
+  if (years > 0) {
+    return `${years} ${years === 1 ? "year" : "years"} ago`;
+  } else if (months > 0) {
+    return `${months} ${months === 1 ? "month" : "months"} ago`;
+  } else if (days > 0) {
+    return `${days} ${days === 1 ? "day" : "days"} ago`;
+  } else if (hours > 0) {
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+  } else if (minutes > 0) {
+    return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+  } else {
+    return `${seconds} ${seconds === 1 ? "second" : "seconds"} ago`;
+  }
+};
+
+export const formatLargeNumber = (number: number): string => {
+  let formattedNumber: string;
+  let divisor: number;
+
+  if (number >= 1000000) {
+    formattedNumber = (number / 1000000).toFixed(1);
+    divisor = 1000000;
+  } else if (number >= 1000) {
+    formattedNumber = (number / 1000).toFixed(1);
+    divisor = 1000;
+  } else {
+    formattedNumber = number.toString();
+    divisor = 1;
+  }
+
+  // Remove trailing '.0' if present
+  formattedNumber = formattedNumber.replace(/\.0$/, "");
+
+  return `${formattedNumber}${
+    divisor === 1 ? "" : divisor === 1000 ? "K" : "M"
+  }`;
+};
